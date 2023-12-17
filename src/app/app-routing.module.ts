d9ad3411857b0813c4af7d24dd208d7a4d468934
sivasuthan manojan <mjcode6@gmail.com>
2023-12-17 20:09:07 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StudentComponent } from './student/student.component';
+import { ErrorComponent } from './error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path: 'home', 
+component:HomeComponent
+ },
+
+ {
+  path: 'wcs/:studentName',
+  component: StudentComponent
+ },
+ {
+  path: '',
+  component: ErrorComponent
+ }
+ 
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
